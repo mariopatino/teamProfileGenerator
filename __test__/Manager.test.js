aaf9cf98ab547bf5e775298ff3377e0493559ba6
Mario Patiño <mariopatino@users.noreply.github.com>
2022-05-01 07:47:57 -0500

    Update Manager.test.js

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -2,18 +2,18 @@ const Employee = require('../lib/Employee');
 const Manager = require('../lib/Manager');
 
 describe('Manager', () => {
-      test('Should instantiate Manager object as an instance of the employee class', () => {
+      test('Creating a Manager object as an instance of the employee class', () => {
             const manager = new Manager();
             expect(manager).toBeInstanceOf(Employee);
       }),
-      test('Should allow the office number property to be set at instantiation', () => {
-            const officeNumber = '1234567890';
-            const manager = new Manager("Manager Name", 1234, "Manager@email.com", officeNumber);
+      test('Addingthe office number property to be set at initialization', () => {
+            const officeNumber = '5533446677';
+            const manager = new Manager("Juan Perez", 123456, "juan@mail.com", officeNumber);
             expect(manager.officeNumber).toBe(officeNumber);
       }),
-      test('getOfficeNumber() should return the office number of Manager object it is called on', () => {
-            const officeNumber = '1234567890';
-            const manager = new Manager("Manager Name", 1234, "Manager@email.com", officeNumber);
+      test('getOfficeNumber() will return the office number of Manager object it is setting on', () => {
+            const officeNumber = '5533446677';
+            const manager = new Manager("Juan Perez", 123456, "juan@mail.com", officeNumber);
             expect(manager.getOfficeNumber()).toBe(officeNumber);
       })
 })
