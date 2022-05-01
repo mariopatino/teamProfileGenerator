e10e140ccfb4f3cabc0e0c74b53487ed61b998e6
Mario Patiño <mariopatino@users.noreply.github.com>
2022-05-01 07:44:35 -0500

    Update Intern.test.js

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -2,18 +2,18 @@ const Employee = require('../lib/Employee');
 const Intern = require('../lib/Intern');
 
 describe('Intern', () => {
-      test('should create an instance of Intern, as a subclass of the employee class', () => {
+      test('creating an Intern objetc, as a subclass of the employee class', () => {
             const intern = new Intern();
             expect(intern).toBeInstanceOf(Employee);
       }),
-      test('should set the school property at instantiation of intern object', () => {
-            const school = "Georgia Institute of Technology";
-            const intern = new Intern("TestName", 1234, "Test@Email.com", school);
+      test('setting the school property at instantiation of intern object', () => {
+            const school = "Universidad Autonoma de Mexico";
+            const intern = new Intern("Cesar Eduardo", 123456, "cesar@mail.com", school);
             expect(intern.school).toBe(school);
       }),
-      test('when called, getSchool() should return the school property supplied at instantiation', () => {
-            const school = "Georgia Institute of Technology";
-            const intern = new Intern("TestName", 1234, "Test@test.com", school);
+      test('calling getSchool() to return the school property provided at creation', () => {
+            const school = "Universidad Autonoma de Mexico"";
+            const intern = new Intern("Cesar", 123456, "cesar@mail.com", school);
             expect(intern.getSchool()).toBe(school);
       })
 })
